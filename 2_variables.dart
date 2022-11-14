999826d0576860fb7bee0414926d446ef6ad3875
Andanter <ankundaandanter@gmail.com>
2022-11-14 15:48:38 -0800

    adding a comment

--- 2_variables.dart
+++ 2_variables.dart
@@ -1,9 +1,10 @@
 void main() {
-  var name = "Ankunda Andante";
+  var name = "Ankunda Andante";//var is a keyword used to decalre a variable in dart
   var regNo = "20/U/2015/EVE";
   var course = "Software Engineering";
 
       print("I am ${name} with registration number of  ${regNo} and i am studying  ${course}.");
       print("I am studying mobile networks & computing this semester.");
+
   
 }
