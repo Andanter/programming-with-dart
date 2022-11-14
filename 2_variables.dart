4de12c4e31bab5e672319c60334e3fe272c197ec
Andanter <ankundaandanter@gmail.com>
2022-11-14 15:44:03 -0800

    File showing variables.

---
+++ 2_variables.dart
@@ -0,0 +1,9 @@
+void main() {
+  var name = "Ankunda Andante";
+  var regNo = "20/U/2015/EVE";
+  var course = "Software Engineering";
+
+      print("I am ${name} with registration number of  ${regNo} and i am studying  ${course}.");
+      print("I am studying mobile networks & computing this semester.");
+  
+}
