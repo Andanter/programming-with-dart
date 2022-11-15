6a9c3252b7b4aa51837bdc63e21905c070789ec4
Andanter <ankundaandanter@gmail.com>
2022-11-14 17:29:35 -0800

    Different datatypes.

---
+++ 11_datatypes.dart
@@ -0,0 +1,37 @@
+//Number datatype
+void main() {
+  int num1 = 10;
+  double num2 = 5.5;
+
+  print(num1);
+  print(num2);
+  var a1 = num.parse("5");
+  var b1 = num.parse("7.55");
+
+  var c1 = a1 * b1;
+  print("Product = ${c1}");
+
+//String datatype
+  String string = 'Learning' ' with';
+  String str = 'Mr. Muhindo is';
+  String str1 = ' Fun';
+  print(string);
+  print(str + str1);
+
+//Boolean datatype
+  String str2 = 'Dart';
+  String str3 = 'is interesting';
+
+  bool val = (str2 == str3);
+  print(val);
+
+
+//Lists datatype
+ List w = new List(3); 
+    w[0] = 'Ankunda'; 
+    w[1] = 'Andante'; 
+    w[2] = 'Rutainama'; 
+    
+    print(w); 
+    print(w[0]);  
+}
