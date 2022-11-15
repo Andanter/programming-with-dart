6dbd88d986aac734748f7810d2909049fa2642b0
Andanter <ankundaandanter@gmail.com>
2022-11-14 17:47:16 -0800

    argumented functions.

---
+++ 13_argumented_functions.dart
@@ -0,0 +1,12 @@
+void main() {
+  getName('Andante', 'Software Engineering');
+  showHobby('She', 'likes swimming');
+}
+
+void getName(String name, String course) {
+  print("$name does $course ");
+}
+
+void showHobby(String noun, String sentence) {
+  print("$noun also $sentence ");
+}
