0387325c45c5a2c10c3918ef0666421acfcbf395
Andanter <ankundaandanter@gmail.com>
2022-11-14 17:13:05 -0800

    Switch statement.

---
+++ 10_switch_statement.dart
@@ -0,0 +1,18 @@
+void main()
+{
+    int k = 10;
+    switch (k) {
+    case 1: {
+        print("First case");
+    } break;
+    case 2: {
+        print("Second case");
+    } break;
+    case 3: {
+        print("Third case");
+    } break;
+    default: {
+        print("This is default case");
+    } break;
+    }
+}
