6fb252a0a5f0873dae32fbe128cd3e811d196ca1
Andanter <ankundaandanter@gmail.com>
2022-11-14 17:37:47 -0800

    Functions example.

---
+++ 12_functions.dart
@@ -0,0 +1,17 @@
+void main() {
+  getName();
+  displayCourse();
+  showRegno();
+}
+
+void getName() {
+  print("My name is Ankunda Andante");
+}
+
+void displayCourse() {
+  print("I do Software Engineering");
+}
+
+void showRegno() {
+  print("My registration number is 20/U/2015/EVE");
+}
